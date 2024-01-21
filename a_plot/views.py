dae7dd2f9c1011bad750cc8a8a9a0ba20e0f86e7
David Allen <spondicious@me.com>
2024-01-21 11:57:06 +0100

    Added the models to for the plots

--- a_plot/views.py
+++ a_plot/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    context = {
+        'title': 'SuperCamper',
+    }
+    return render(request, "index.html", context)
 
 
