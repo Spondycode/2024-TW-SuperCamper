f4844e3febf68a2bcb030625073de8ac2eb99883
David Allen <spondicious@me.com>
2024-02-10 16:20:38 +0100

    Refactor search user functionality in form_searchuser.html and add search_users view

--- a_inbox/urls.py
+++ a_inbox/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import inbox_view
+from .views import inbox_view, search_users
 
 
 urlpatterns = [
     path('', inbox_view, name="inbox"),
     path('c/<conversation_id>/', inbox_view, name="inbox"),
+    path('search_users/', search_users, name="inbox-searchusers" )
 ]
--- a_inbox/views.py
+++ a_inbox/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Conversation
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 @login_required
 def inbox_view(request, conversation_id=None):
@@ -14,3 +16,16 @@ def inbox_view(request, conversation_id=None):
         "my_conversations": my_conversations,
     }
     return render(request, "a_inbox/inbox.html", context)
+
+
+def search_users(request):
+    letters = request.GET.get("search_user")
+    if len(letters) > 0:
+        users = User.objects.filter(username__startswith=letters).exclude(username=request.user.username)
+        context = {
+            "users": users,
+        }
+        return render(request, "a_inbox/list_searchuser.html", context)
+    else:
+        return HttpResponse("")
+    
