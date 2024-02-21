0bb4b3de69024dc60c8f21f72717f4c323c704fe
David Allen <spondicious@me.com>
2024-02-21 22:34:03 +0100

    Update ENVIRONMENT setting to production

--- a_core/settings.py
+++ a_core/settings.py
@@ -9,10 +9,10 @@ from environ import Env
 
 env = Env()
 # set casting, default value
-DEBUG=(bool, False))
+# DEBUG=(bool, False))
 # reading .env file
 Env.read_env()
-ENVIRONMENT = env('ENVIRONMENT', default='development')
+ENVIRONMENT = env('ENVIRONMENT', default='production')
 
 SECRET_KEY = env('SECRET_KEY')
 ENCRYPT_KEY = env('ENCRYPT_KEY')
