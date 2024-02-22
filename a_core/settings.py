1947a89250675f0eb7f877578795821aeb97ab74
David Allen <spondicious@me.com>
2024-02-22 10:29:10 +0100

    Update database configuration in settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -125,7 +125,7 @@ DATABASES = {
 }
 
 POSTGRES_LOCALLY = False
-if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
+if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:  # noqa: E712
     DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
 
